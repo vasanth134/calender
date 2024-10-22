ff8a330dbae9646f3e57e82278b0b330b60a77cd
vasanth134 <crazyvasanth004@gmail.com>
2024-10-22 17:03:22 +0530

    Update package dependencies and add date click handler to Calendar component

--- custom-calendar/src/component/Calendar.jsx
+++ custom-calendar/src/component/Calendar.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import FullCalendar from "@fullcalendar/react"
-import DayGridPlugin from "@fullcalendar/daygrid"
-import TimeGridPlugin from "@fullcalendar/timegrid"
-import IntertactionPlugin from "@fullcalendar/interaction"
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import DayGridPlugin from "@fullcalendar/daygrid";
+import TimeGridPlugin from "@fullcalendar/timegrid";
+import IntertactionPlugin from "@fullcalendar/interaction";
 function Calendar() {
+  
+  const handleDateClick = (arg) =>{
+    alert(arg.dateStr)
+  }
+  
+  
   return (
- <>
- <FullCalendar plugins={[DayGridPlugin,TimeGridPlugin,IntertactionPlugin]}
- initialView={'dayGridMonth'} 
- headerToolbar={
-    {
-        start: "today prev,next",
-        center:"title",
-        end:"dayGridMonth, timeGridWeek, timeGridDay"
-    }
- }
- height={"98vh"}
- />
- </>
-  )
+    <>
+      <FullCalendar
+        plugins={[DayGridPlugin, TimeGridPlugin, IntertactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "today prev,next",
+          center: "title",
+          end: "dayGridMonth, timeGridWeek, timeGridDay", 
+        }
+      }
+      dateClick={handleDateClick}
+        height={"98vh"}
+      />
+    </>
+  );
 }
-
-export default Calendar
+export default Calendar;
